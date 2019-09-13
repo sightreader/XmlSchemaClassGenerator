957617e0a6602f774f9c68e3796c50f2eb4da62e
Jason Pang <jppang@ucdavis.edu>
2019-09-12 17:06:16 -1000

    Add example program to generate MusicXML schema
    Based on the original project's README example code. This successfully
    generates a XSD -> C# code where names are pascal cased and not
    underscored.

---
+++ ExampleProgram/Program.cs
@@ -0,0 +1,34 @@
+﻿using XmlSchemaClassGenerator;
+using System;
+using System.Collections.Generic;
+
+namespace ExampleProgram
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var generator = new Generator
+            {
+                OutputFolder = "output",
+                Log = s => Console.WriteLine(s),
+                GenerateNullables = false,
+                PrivateMemberPrefix = "",
+                NamespaceProvider = new Dictionary<NamespaceKey, string>
+                {
+                    { new NamespaceKey("http://wadl.dev.java.net/2009/02"), "MusicXmlSchema" }
+                }
+                .ToNamespaceProvider(new GeneratorConfiguration { NamespacePrefix = "MusicXmlSchema" }.NamespaceProvider.GenerateNamespace)
+            };
+
+            try
+            {
+                generator.Generate(new string[] { "musicxml.xsd" });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.ToString()}");
+            }
+        }
+    }
+}
